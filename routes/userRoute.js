1000d45f200fb2525f4b87a3caa4d3a7adc4eb10
Muhammed-Rizin <Rizin7427@gmail.com>
2023-06-21 17:04:28 +0530

    problem with image uploading solved

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,34 @@ const express = require('express')
 const userRoute = express()
 const userController = require('../controller/userController')
 const multer = require('multer')
-const upload = multer({dest : './file'})
+// const upload = multer({dest : './file'})
+const path = require('path')
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,path.join(__dirname, '../file'))
+    },
+    filename:function(req,file,cb){
+        const name = Date.now()+'-'+file.originalname;
+        cb(null,name)
+    }
+})
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+      if (
+        file.mimetype == "image/png" ||
+        file.mimetype == "image/jpg" ||
+        file.mimetype == "image/jpeg" ||
+        file.mimetype == "image/webp" 
+        
+      ) {
+        cb(null, true);
+      } else {
+        cb(null, false);
+        return cb(new Error("Only .png, .jpg and .jpeg .webp format allowed!"));
+      }
+    },
+  });
 
 // Register
 userRoute.post('/register', userController.postRegister)
